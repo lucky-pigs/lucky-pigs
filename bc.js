48e0fd86d087b3c17ebe45e1fa3b4d906c06f208
lucky-pigs <99507120+lucky-pigs@users.noreply.github.com>
2022-02-12 09:56:26 +0100

    Update bc.js

--- bc.js
+++ bc.js
@@ -5,7 +5,7 @@ if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
     getAccount();
 } else {
-    alert("You need a browser with Metamask to use xALT.")
+    alert("You need a browser with Metamask to partecipate!")
 }
 
 pork_address = "0x9BfdB3B417F334953f94324463129cb00e6E4E87";
@@ -41,11 +41,45 @@ async function start_app(account) {
 }
 
 async function loadLastPorks(lastpig = 800){
-    console.log("Loading porks");
-    "https://gateway.pinata.cloud/ipfs/QmWvGSt2pUDRSuZc6gB6by4532SgjKv8kKiunrSijeTEpS/1.png"
-    799
-    798
-    797
+
+    lastMintedPork();
+    loadStats();
+
+}
+
+last_pork_index = 0;
+current_price = 0;
+current_max_prod = 0;
+next_price = 0;
+
+async function lastMintedPork(){
+        last_pork_index = await pork_contract.methods.totalSupply().call();
+        console.log(last_pork_index);
+        supply = last_pork_index;
+        if      (supply < 100)  {current_price = 0.005; current_max_prod = 100;next_price = 0.01;}
+        else if (supply < 1000) {current_price = 0.01; current_max_prod = 900;next_price = 0.02;}
+        else if (supply < 2000) {current_price = 0.02; current_max_prod = 1000;next_price = 0.04;}
+        else if (supply < 3000) {current_price = 0.04; current_max_prod = 1000;next_price = 0.08;}
+        else if (supply < 4000) {current_price = 0.08; current_max_prod = 1000;next_price = 0.08;}
+        else if (supply < 5000) {current_price = 0.08; current_max_prod = 1000;next_price = 0.1;}
+        else if (supply < 6000) {current_price = 0.1; current_max_prod = 1000;next_price = 0.1;}
+        else if (supply < 7000) {current_price = 0.1; current_max_prod = 1000;next_price = 0.15;}
+        else if (supply < 9000) {current_price = 0.15; current_max_prod = 1000;next_price = 0.15;}
+        else                    {current_price = 9;}
+        return last_pork_index;
+}
+
+async function loadStats(){
+    addr_balance = await web3.eth.getBalance(pork_address);
+    addr_balance = Number(web3.utils.fromWei(addr_balance)).toFixed(3);
+    console.log(addr_balance);
+    document.getElementById('jackpot-eth').innerHTML = addr_balance + "<i class='fab fa-ethereum'></i> Locked";
+    document.getElementById('pork-counter').innerHTML = current_max_prod-(last_pork_index%current_max_prod);
+    document.getElementById('pork-price').innerHTML = current_price;
+    document.getElementById('curr-price').innerHTML = current_price;
+    document.getElementById('next-price').innerHTML = next_price;
+
+
 
 }
 
@@ -53,7 +87,7 @@ async function mintporks(amount=1){
 
 
     pork_contract.methods.publicSaleMint(amount)
-        .send({from: user_main_address,value:web3.utils.toWei("0.01", "ether")}, function(error, transactionHash){
+        .send({from: user_main_address,value:web3.utils.toWei(String(current_price*amount), "ether")}, function(error, transactionHash){
             console.log(error);
             console.log(transactionHash);
         });
